05691f7b63ba897d1a1763eefbabbcaa83791d70
venkatish1992 <venkatish1992@gmail.com>
2024-02-11 21:00:00 +0530

    first commit

---
+++ build/lib/getTitles/spiders/get_titles.py
@@ -0,0 +1,19 @@
+import scrapy
+import os
+
+class TitleSpider(scrapy.Spider):
+    name = "title"
+    #get the list of domains from the file
+    with open('D:\misc\ezoic domains\getTitles\data\domains.txt', 'r') as f:
+        start_urls = f.readlines()
+    #add the http:// to each domain
+    start_urls = ['http://' + url.strip() for url in start_urls]
+
+
+    def parse(self, response):
+        self.log('Visited %s' % response.url)
+        yield {
+            'title': response.css('title::text').extract_first(),
+            'url': response.url
+        }
+
